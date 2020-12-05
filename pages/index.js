11bc16637f2668aeed295c0c39100c71c0af66f5
Felipe Antoniati <antoniati.felipe@gmail.com>
2020-12-05 12:09:40 -0300

    test next/link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home() {
-  return <h1>Hello World</h1>;
-}
+import Link from "next/link";
 
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/sobre">
+        <a>Acessar página Sobre</a>
+      </Link>
+    </div>
+  );
+}
 export default Home;

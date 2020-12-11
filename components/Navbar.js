2c85988b0d2332f89a8e1c4a44a4689c67eb5c05
felipe-antoniati <antoniati.felipe@gmail.com>
2020-12-11 14:32:58 -0300

    modification of the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,71 +1,91 @@
+import React, { useState } from "react";
 import Link from "next/link";
+import { SidebarData } from "./SidebarData";
+
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
-import React, { useState } from "react";
-import { SidebarData } from "./SidebarData";
+import { IconContext } from "react-icons";
+import perfilImg from "../images/perfil.png";
 
 function Navbar() {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
 
   return (
-    <div className="wrapper">
-      <div className="container flex">
-        <div className="social">
-          <Link href="#">
-            <a>
-              <FaIcons.FaGithub size={30} color="#FBFBFF" />
-            </a>
-          </Link>
-          <Link href="#">
-            <a>
-              <FaIcons.FaLinkedin size={30} color="#FBFBFF" />
-            </a>
-          </Link>
-        </div>
-        <div className="menu-bars">
-          <Link href="#">
-            <a>
-              <FaIcons.FaBars onClick={showSidebar} size={30} color="#FBFBFF" />
-            </a>
-          </Link>
-        </div>
-
-        <nav className="navbar">
-          <ul className="nav-menu">
-            <li className="nav-item">
-              <Link href="/">
-                <a>Inicio</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/services">
-                <a>Serviços</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/portifolio">
-                <a>Portifolio</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/portifolio">
-                <a>Sobre</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      <div className="nav-sidebar-menu">
-        <nav className={sidebar ? "nav-sidebar active" : "nav-hamburguer"}>
-          <ul className="nav-sidebar-items" onClick={showSidebar}>
-            <li className="navbar-toggle">
-              <Link href="#" className="menu-bars">
+    <>
+      <IconContext.Provider value={{ color: "#FFF" }}>
+        <div className="navbar">
+          <div className="nav-left">
+            <div className="social">
+              <Link href="#">
                 <a>
-                  <AiIcons.AiOutlineClose />
+                  <FaIcons.FaGithub size={30} />
                 </a>
               </Link>
-            </li>
+              <Link href="#">
+                <a>
+                  <FaIcons.FaLinkedin size={30} />
+                </a>
+              </Link>
+            </div>
+          </div>
+          <div className="nav-rigth">
+            <div className="menu-bars">
+              <Link href="#">
+                <a>
+                  <FaIcons.FaBars onClick={showSidebar} size={30} />
+                </a>
+              </Link>
+            </div>
+            <div className="navbar-menu">
+              <ul className="nav-list">
+                <li className="nav-item">
+                  <Link href="/">
+                    <a>Inicio</a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link href="/services">
+                    <a>Serviços</a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link href="/portifolio">
+                    <a>Portifolio</a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link href="/portifolio">
+                    <a>Sobre</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+          <ul className="nav-menu-items" onClick={showSidebar}>
+            <div className="header-navbar">
+              <div className="social">
+                <Link href="#">
+                  <a>
+                    <FaIcons.FaGithub size={30} />
+                  </a>
+                </Link>
+                <Link href="#">
+                  <a>
+                    <FaIcons.FaLinkedin size={30} />
+                  </a>
+                </Link>
+              </div>
+              <li className="navbar-toggle">
+                <Link href="#" className="menu-bars">
+                  <a>
+                    <AiIcons.AiOutlineClose size={30} />
+                  </a>
+                </Link>
+              </li>
+            </div>
             {SidebarData.map((item, index) => {
               return (
                 <li key={index} className={item.cName}>
@@ -79,8 +99,8 @@ function Navbar() {
             })}
           </ul>
         </nav>
-      </div>
-    </div>
+      </IconContext.Provider>
+    </>
   );
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/utilities.css";
 import "../styles/global.css";
+import "../styles/navbar.css";
 
 function MyApp({ Component, pageProps }) {
   return (
